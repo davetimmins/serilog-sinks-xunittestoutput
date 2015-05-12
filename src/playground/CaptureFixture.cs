f2323a6bf53e812a427bf167ddace302d8d2ebf4
Dave Timmins <dwtimmins@outlook.com>
2015-05-12 12:25:36 +1200

    Initial commit

---
+++ src/playground/CaptureFixture.cs
@@ -0,0 +1,43 @@
+﻿namespace playground
+{
+    using Serilog;
+    using System;
+    using Xunit;
+    using Xunit.Abstractions;
+
+    public class CaptureFixture
+    {
+        public void Capture()
+        { }
+
+        public void ConfigureLogging(ITestOutputHelper output)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.XunitTestOutput(output)
+                .CreateLogger();
+        }
+    }
+
+    [CollectionDefinition("Capture")]
+    public class CaptureTestCollection : ICollectionFixture<CaptureFixture> { }
+
+    [Collection("Capture")]
+    public abstract class CaptureTests : IDisposable
+    {
+        CaptureFixture _fixture;
+        ITestOutputHelper _output;
+
+        protected CaptureTests(CaptureFixture fixture, ITestOutputHelper output)
+        {
+            _fixture = fixture;
+            _output = output;
+
+            _fixture.ConfigureLogging(_output);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ src/playground/Tests.cs
@@ -0,0 +1,57 @@
+﻿namespace playground
+{
+    using playground.Logging;
+    using Xunit;
+    using Xunit.Abstractions;
+
+    public class Tests1 : CaptureTests
+    {
+        public Tests1(CaptureFixture fixture, ITestOutputHelper output)
+            : base(fixture, output) { }
+
+        [Fact]
+        public void FirstTest()
+        {
+            new NewThing("First", LogProvider.For<Tests1>());
+            Assert.True(true);
+        }
+    }
+
+    public class Tests2 : CaptureTests
+    {
+        public Tests2(CaptureFixture fixture, ITestOutputHelper output)
+            : base(fixture, output) { }
+
+        [Fact]
+        public void SecondTest()
+        {
+            new NewThing("Second", LogProvider.For<Tests2>());
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void ThirdTest()
+        {
+            new NewerThing("Third");
+            Assert.True(true);
+        }
+    }
+
+    class NewThing
+    {
+        public NewThing (string message, ILog log)
+	    {
+            log.Info(message);
+	    }
+    }
+
+    class NewerThing
+    {
+        readonly ILog log;
+        public NewerThing(string message)
+        {
+            log = LogProvider.For<NewerThing>();
+            log.Info(message);
+        }
+    }
+}
